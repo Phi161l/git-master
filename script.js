f4ee9fcbf17a35b819c33732114d32e92e900596
ibraX <ibrahimnaik91@gmail.com>
2026-06-03 20:11:26 +0300

    seventh commit

---
+++ script.js
@@ -0,0 +1,14 @@
+console.log("Feature 1 branch - JavaScript file added");
+
+function greet(name) {
+  return `Hello, ${name}`;
+}
+
+console.log(greet("Git Learner"));
+
+// New feature added
+function add(a, b) {
+  return a + b;
+}
+
+console.log("Sum:", add(5, 3));
