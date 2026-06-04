cf6868d18023670d2b70108b6a8da9f8859b13bd
ibraX <ibrahimnaik91@gmail.com>
2026-06-04 15:44:18 +0300

    1st commit on feature-1 after merge to fix

--- data.js
+++ data.js
@@ -2,3 +2,7 @@ console.log("New file added in feature-1 branch");
 
 const version = "1.1";
 console.log("App version:", version);
+
+const appName = "Git Practice App";
+
+console.log("Application:", appName);
